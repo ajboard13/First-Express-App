da50264e80be19f7b7a2efb2f9cb8ffedd1218a7
Aaron Board <ajboard13@gmail.com>
2019-12-18 15:23:19 -0500

    adding a pattern matched route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
 
 app.get('/api', function(req, res){
     res.json({firstname:'John', lastname:'Doe'});
-})
+});
+
+app.get('/person/:id', function(req, res){
+    res.send('<html><head></head><body><h1>Hello '+ req.params.id +'!!!</h1></body></html>');
+});
 
 app.listen(port);
