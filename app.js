c1e8d3385b9f93dd0da3cca404b1cd9e0f2474d7
Aaron Board <ajboard13@gmail.com>
2019-12-19 14:45:25 -0500

    first connection to a MongDB instance

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+mongoose.connect('mongodb+srv://aaron:test@first-mongodb-lx5vf.mongodb.net/test?retryWrites=true&w=majority');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+    firstname: 'John',
+    lastname: 'Doe',
+    address: ' 555 Main St'
+});
+
+john.save(function(err)  {
+    if(err) throw err;
+
+    console.log('John saved!');
+});
 
 var port = process.env.PORT || 3000;
 
@@ -8,6 +32,12 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 app.use('/', function(req,res,next){
     console.log('Request Url: ' + req.url);
+
+    Person.find({},  function(err,users) {
+        if (err) throw err;
+
+        console.log(users);
+    });
     next();
 })
 
@@ -20,7 +50,7 @@ app.get('/api', function(req, res){
 });
 
 app.get('/person/:id', function(req, res){
-    res.send('<html><head></head><body><h1>Hello '+ req.params.id +'!!!</h1></body></html>');
+    res.send('<html><head></head><body><h1>Hello '+ req.params.id +'!!!' + req.query.qstr + '</h1></body></html>');
 });
 
 app.listen(port);
